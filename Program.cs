cf030f47ce7bde5f0a9c2d6743759245a76fe5e2
Karol Poznański <pl.youuw@gmail.com>
2023-11-09 09:01:57 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace programowanie_obiektowe3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
